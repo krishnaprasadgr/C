bdf87a459f5a7cd1245492dfc04092327f733695
krishna <47688157+krishnaprasadgr@users.noreply.github.com>
2019-11-05 23:47:32 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,28 @@
+/*
+C Program to Check Whether a Number is Palindrome or Not
+This program reverses an integer (entered by the user) using while loop. 
+Then, if statement is used to check whether the reversed number is equal to the original number or not.
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    originalInteger = n;
+    // reversed integer is stored in variable 
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reversedInteger = reversedInteger*10 + remainder;
+        n /= 10;
+    }
+    // palindrome if orignalInteger and reversedInteger are equal
+    if (originalInteger == reversedInteger)
+        printf("%d is a palindrome.", originalInteger);
+    else
+        printf("%d is not a palindrome.", originalInteger);
+    
+    return 0;
+}
