0fbafdd98bbfb4169ee7bcbcc7e10fd6e7bd4eef
krishna <47688157+krishnaprasadgr@users.noreply.github.com>
2019-11-05 23:58:19 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,7 +1,7 @@
 /*
 C Program to Display Fibonacci Sequence
 Example on how to display the Fibonacci sequence of first n numbers (entered by the user) using loop.
-Also in different example, you learn to generate the Fibonacci sequence up to a certain number
+Also in different example, you learn to generate the Fibonacci sequence up to a certain number.
 */
 
 #include <stdio.h>
