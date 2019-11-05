a6d89050ccc947135b00677dea5b1e685725ca64
krishna <47688157+krishnaprasadgr@users.noreply.github.com>
2019-11-05 23:50:16 +0530

    Create Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,28 @@
+/*
+C Program to Check Armstrong Number
+Example to check whether an integer (entered by the user) is an Armstrong number or not using while loop and if...else statement.
+
+In case of an Armstrong number of 3 digits, the sum of cubes of each digits is equal to the number itself. For example:
+153 = 1*1*1 + 5*5*5 + 3*3*3  // 153 is an Armstrong number.
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int number, originalNumber, remainder, result = 0;
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+    originalNumber = number;
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+    return 0;
+}
